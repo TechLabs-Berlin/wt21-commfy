95f7a827a7c7c6bb75f2e32a5fc57699ef2fae38
Matheus Albuquerque <land@ythecombinator.space>
2021-12-30 15:46:18 +0100

    feat(client/src/pages/AuthLogin/AuthLogin.tsx): use new auth hook

--- client/src/pages/AuthLogin/AuthLogin.tsx
+++ client/src/pages/AuthLogin/AuthLogin.tsx
@@ -9,24 +9,27 @@ import {
   IonInput,
   IonButton,
   IonIcon,
-  IonAlert,
 } from "@ionic/react";
-import React, { useState } from "react";
+import React from "react";
 import { IonGrid, IonRow, IonCol } from "@ionic/react";
 import { personCircle } from "ionicons/icons";
-import { useHistory } from "react-router-dom";
-import { useFormik } from "formik";
+import { Formik, FormikConfig } from "formik";
+import { useAuthentication } from "../../utils/firebase";
 
 const AuthLogin: React.FC = () => {
-  const { values, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      password: "",
-      email: "",
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+  const { signIn } = useAuthentication();
+
+  // const username = "matheus@gmail.com";
+  // const pass = "12345678";
+
+  const onSubmit: FormikConfig<any>["onSubmit"] = async (
+    values,
+    { setSubmitting }
+  ) => {
+    const user = await signIn(values.email, values.password);
+    console.log("user", user);
+    setSubmitting(false);
+  };
 
   return (
     <IonPage>
@@ -37,66 +40,81 @@ const AuthLogin: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen className="ion-padding ion-text-center">
         <IonGrid>
-          {/* <IonRow>
-            <IonCol>
-              <IonAlert
-                isOpen={iserror}
-                onDidDismiss={() => setIserror(false)}
-                cssClass="my-custom-class"
-                header={"Error!"}
-                message={message}
-                buttons={["Dismiss"]}
-              />
-            </IonCol>
-          </IonRow> */}
-          <IonRow>
-            <IonCol>
-              <IonIcon
-                style={{ fontSize: "70px", color: "#0040ff" }}
-                icon={personCircle}
-              />
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonLabel position="floating">Email</IonLabel>
-                <IonInput
-                  type="email"
-                  name="email"
-                  value={values.email}
-                  onIonInput={handleChange}
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={onSubmit}
+          >
+            {({
+              values,
+              // errors,
+              // touched,
+              // isSubmitting,
+              handleChange,
+              handleSubmit,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                {/* <IonRow>
+                  <IonCol>
+                    <IonAlert
+                      isOpen={}
+                      onDidDismiss={}
+                      header={"Error!"}
+                      message={message}
+                      buttons={["Dismiss"]}
+                    />
+                  </IonCol>
+                </IonRow> */}
+                <IonRow>
+                  <IonCol>
+                    <IonIcon
+                      style={{ fontSize: "70px", color: "#0040ff" }}
+                      icon={personCircle}
+                    />
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem>
+                      <IonLabel position="floating">Email</IonLabel>
+                      <IonInput
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onIonInput={handleChange}
+                      ></IonInput>
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
 
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonLabel position="floating">Password</IonLabel>
-                <IonInput
-                  type="password"
-                  name="password"
-                  value={values.password}
-                  onIonInput={handleChange}
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <p style={{ fontSize: "small" }}>
-                By clicking LOGIN you agree to our <a href="#">Policy</a>
-              </p>
-              <IonButton expand="block" onClick={() => handleSubmit()}>
-                Login
-              </IonButton>
-              <p style={{ fontSize: "medium" }}>
-                Don't have an account? <a href="#">Sign up!</a>
-              </p>
-            </IonCol>
-          </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem>
+                      <IonLabel position="floating">Password</IonLabel>
+                      <IonInput
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onIonInput={handleChange}
+                      ></IonInput>
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <p style={{ fontSize: "small" }}>
+                      By clicking LOGIN you agree to our <a href="#">Policy</a>
+                    </p>
+                    <IonButton type="submit" expand="block">
+                      Login
+                    </IonButton>
+                    <p style={{ fontSize: "medium" }}>
+                      Don't have an account? <a href="#">Sign up!</a>
+                    </p>
+                  </IonCol>
+                </IonRow>
+              </form>
+            )}
+          </Formik>
         </IonGrid>
       </IonContent>
     </IonPage>
