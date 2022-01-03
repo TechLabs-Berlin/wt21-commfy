c5edde594117f3efb140b52c33880db027c763a8
Kevin Cummings <adfontes@protonmail.com>
2022-01-03 11:00:08 +0100

    Added WeatherComponent, IonCard in Tab1

---
+++ client/src/components/WeatherAPI/Weather.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+
+
+export const Weather: FunctionComponent = () => {
+    return (
+        <div>WeatherAPI fetch result goes here!</div>
+    );
+};
--- client/src/pages/Tab1.tsx
+++ client/src/pages/Tab1.tsx
@@ -1,25 +1,42 @@
 import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "components/ExploreContainer";
-import { useUser } from "utils/firebase";
+import { Weather } from "components/WeatherAPI/Weather";
+import { doc } from "firebase/firestore";
+import { useFirestore, useFirestoreDocData } from "reactfire";
+import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 
 const Tab1: React.FC = () => {
-  const { data, loading } = useUser("As8FrfQrJjBJJiO6u6aX");
-  console.log(data, loading);
+  // access the Firestore library
+  const userRef = doc(useFirestore(), "users", "As8FrfQrJjBJJiO6u6aX");
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData(userRef);
+
+  console.log(data, status);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
+          <IonCard>
+            <IonCardContent>
+            <Weather />
+            </IonCardContent>
+          </IonCard>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
